af3b3b4f91b484be4571808266f628391f8b7951
Sunil Meghwal <sunillmeghwal@gmail.com>
2025-08-26 10:53:17 +0530

    http-server

--- node_tutorial/02-HTTP/index.js
+++ node_tutorial/02-HTTP/index.js
@@ -1,44 +1,34 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
-    console.log(`Incomming request at [${Date.now()}]`);
-    // console.log(req.headers);
-    // console.log(req.method);
-    console.log(req.url);
+  console.log(`Incomming request at [${Date.now()}]`);
+  // console.log(req.headers);
+  // console.log(req.method);
+  console.log(req.url);
 
+  //..
 
-     //..
+  // res.writeHead(200);
+  // // res.end('OK!');
+  // res.end(`Hey, you can accept ${req.headers['accept-language']}`);
 
-    // res.writeHead(200);
-    // // res.end('OK!');
-    // res.end(`Hey, you can accept ${req.headers['accept-language']}`);
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      return res.end("Homepage");
 
+    case "/contact-us":
+      res.writeHead(200);
+      return res.end("Contact Me at sunilmeghwal@gmail.com");
 
+    case "/about":
+      res.writeHead(200);
+      return res.end("I am a webdeveloper");
 
-    switch(req.url){
-
-        case '/':
-            res.writeHead(200);
-            return res.end('Homepage')
-          
-            case '/contact-us':
-                res.writeHead(200);
-                 return res.end('Contact Me at sunilmeghwal@gmail.com')
-                 
-                 case '/about':
-                    res.writeHead(200);
-                     return res.end('I am a webdeveloper')
-                     
-
-                     default:
-                        res.writeHead(400);
-                        return res.end("You're lost")
-
-
-    }
-    
-
-   
+    default:
+      res.writeHead(400);
+      return res.end("You're lost");
+  }
 });
 
 server.listen(8000, () => console.log(`Server is running on Port: 8000`));
